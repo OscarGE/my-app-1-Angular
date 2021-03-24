0614ced939b30cf82e0a8cfaf006820242fffcd1
Oscar <oscarge199902@gmail.com>
2021-03-23 20:51:27 -0600

    Se eliminaron las carpetas de los componenetes y se agregaron estilos globales y se editó el componente raíz

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { MiComponente1Component } from './mi-componente1/mi-componente1.component';
-import { MiComponente2Component } from './mi-componente2/mi-componente2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MiComponente1Component,
-    MiComponente2Component,
   ],
   imports: [
     BrowserModule
